e2b6117150e94e3ea6d4d26622af096b31cf31f9
Oluwadamilare <joeydre69@gmail.com>
2024-03-25 22:19:21 +0100

    stage six

--- app/server.go
+++ app/server.go
@@ -15,40 +15,45 @@ func main() {
 
 	ln, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("Failed to bind to port 4221", err)
 		os.Exit(1)
 	}
 	defer ln.Close()
-	conn, err := ln.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err)
-		os.Exit(1)
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err)
+			os.Exit(1)
+		}
+		go func() {
+			buf := make([]byte, 1024)
+			_, err = conn.Read(buf)
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err)
+			}
+			req := string(buf)
+			lines := strings.Split(req, CRLF)
+			path := strings.Split(lines[0], " ")[1]
+			fmt.Println(path)
+			var res string
+			if path == "/" {
+				res = "HTTP/1.1 200 OK\r\n\r\n"
+			} else if strings.HasPrefix(path, "/echo/") {
+				msg := path[6:]
+				res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(msg), msg)
+			} else if path == "/user-agent" {
+				msg := strings.Split(lines[2], " ")[1]
+				res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(msg), msg)
+			} else {
+				res = "HTTP/1.1 404 Not Found\r\n\r\n"
+			}
+			fmt.Println(res)
+			conn.Write([]byte(res))
+			if err != nil {
+				fmt.Println("Error accepting connection: ", err)
+				os.Exit(1)
+			}
+		}()
 	}
-	buf := make([]byte, 1024)
-	_, err = conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err)
-	}
-	req := string(buf)
-	lines := strings.Split(req, CRLF)
-	path := strings.Split(lines[0], " ")[1]
-	fmt.Println(path)
-	var res string
-	if path == "/" {
-		res = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if strings.HasPrefix(path, "/echo/") {
-		msg := path[6:]
-		res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(msg), msg)
-	} else if path == "/user-agent" {
-		msg := strings.Split(lines[2], " ")[1]
-		res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %v\r\n\r\n%v", len(msg), msg)
-	} else {
-		res = "HTTP/1.1 404 Not Found\r\n\r\n"
-	}
-	fmt.Println(res)
-	conn.Write([]byte(res))
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err)
-		os.Exit(1)
-	}
+
 }
